8d5c769a9cb46b2a78f42d7277d069aa3cdc7a5e
Xinda Huang <XindaH@github.com>
2018-11-14 01:16:28 -0600

    delete dm and change set to map

--- src/edu/nu/xinda/pages/LogIn.java
+++ src/edu/nu/xinda/pages/LogIn.java
@@ -7,16 +7,18 @@ import edu.nu.xinda.core.DatabaseManager;
 import java.io.IOException;
 import java.sql.*;
 
-public class LogIn implements Page{
+public class LogIn implements Page {
     private static LogIn instance;
     private int currentStudentId;
     private String currentStudentName;
-    private DatabaseManager dm= DatabaseManager.getInstance();
-    private Connection conn=dm.getConn();
+    private Connection conn;
 
-    private LogIn(){}
-    public static LogIn getInstance(){
-        if(instance == null){
+    private LogIn() {
+        conn = DatabaseManager.getInstance().getConn();
+    }
+
+    public static LogIn getInstance() {
+        if (instance == null) {
             instance = new LogIn();
         }
         return instance;
@@ -31,7 +33,8 @@ public class LogIn implements Page{
     }
 
     @Override
-    public void onEnter() {}
+    public void onEnter() {
+    }
 
     @Override
     public void printPageInfo() {
@@ -40,23 +43,23 @@ public class LogIn implements Page{
 
     @Override
     public MainLoop.Position execCommand(String command) {
-        String[] input=command.split(" ");
-        Statement stat= null;
-        ResultSet rs=null;
+        String[] input = command.split(" ");
+        Statement stat = null;
+        ResultSet rs = null;
         try {
             stat = conn.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         try {
-            String sql="select * from student where Id="+input[0]+" and Password='"+input[1]+"'";
-            rs=stat.executeQuery(sql);
-            if(rs.first()){
+            String sql = "select * from student where Id=" + input[0] + " and Password='" + input[1] + "'";
+            rs = stat.executeQuery(sql);
+            if (rs.first()) {
                 currentStudentId = rs.getInt(1);
                 currentStudentName = rs.getString(2);
                 rs.close();
                 return MainLoop.Position.MAIN_MENU;
-            }else{
+            } else {
                 rs.close();
                 System.out.println("Sorry, this does not match our records. Check the spelling and try again.");
             }
--- src/edu/nu/xinda/pages/MainMenu.java
+++ src/edu/nu/xinda/pages/MainMenu.java
@@ -15,89 +15,83 @@ import java.util.*;
  * Package: edu.nu.xinda.pages
  */
 public class MainMenu implements Page {
-	private static MainMenu instance;
-	private DatabaseManager dm= DatabaseManager.getInstance();
-	private Connection conn=dm.getConn();
+    private static MainMenu instance;
+    private Connection conn;
+    private Map<String, MainLoop.Position> map;
 
-	private MainMenu() {}
+    private MainMenu() {
+        conn = DatabaseManager.getInstance().getConn();
+        map = new HashMap<>();
+        map.put("1", MainLoop.Position.TRANSCRIPT);
+        map.put("2", MainLoop.Position.ENROLL);
+        map.put("3", MainLoop.Position.WITHDRAW);
+        map.put("4", MainLoop.Position.PERSONAL_DETAILS);
+        map.put("5", MainLoop.Position.STARTED);
+        map.put("6", MainLoop.Position.EXIT);
 
-	public static MainMenu getInstance() {
-		if (instance == null) {
-			instance = new MainMenu();
-		}
-		return instance;
-	}
-	private String getQuarter(int month){
-	    String q=null;
-	    if(month>=10 && month<=12) q="Q1";
-        else if(month>=1 && month<=3) q="Q2";
-        else if(month>=4 && month<=6) q="Q3";
-        else if(month>=7 && month<=9) q="Q4";
+    }
+
+    public static MainMenu getInstance() {
+        if (instance == null) {
+            instance = new MainMenu();
+        }
+        return instance;
+    }
+
+    private String getQuarter(int month) {
+        String q = null;
+        if (month >= 10 && month <= 12) q = "Q1";
+        else if (month >= 1 && month <= 3) q = "Q2";
+        else if (month >= 4 && month <= 6) q = "Q3";
+        else if (month >= 7 && month <= 9) q = "Q4";
         return q;
     }
 
-	@Override
-	public void onEnter() {
-		// use Login.getInstance().getCurrentStudentId() and query student info
-        int id=LogIn.getInstance().getCurrentStudentId();
-        String name=LogIn.getInstance().getCurrentStudentName();
-        System.out.print("Hi, "+name+"!"+"\n\n");
+    @Override
+    public void onEnter() {
+        // use Login.getInstance().getCurrentStudentId() and query student info
+        int id = LogIn.getInstance().getCurrentStudentId();
+        String name = LogIn.getInstance().getCurrentStudentName();
+        System.out.print("Hi, " + name + "!" + "\n\n");
         System.out.print("Main Menu\n\n");
-        int year=Calendar.getInstance().get(Calendar.YEAR);
-        int month=Calendar.getInstance().get(Calendar.MONTH);
-        String quarter=getQuarter(month);
-        String sql="select unitofstudy.UoSCode,UoSname from transcript,unitofstudy where Semester='"+quarter+"'" +
-                " and year=" +year+" and transcript.StudId="+id+" and unitofstudy.uoscode=transcript.uoscode;";
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        String quarter = getQuarter(month);
+        String sql = "select unitofstudy.UoSCode,UoSname from transcript,unitofstudy where Semester='" + quarter + "'" +
+                " and year=" + year + " and transcript.StudId=" + id + " and unitofstudy.uoscode=transcript.uoscode;";
         try {
-            Statement stat=conn.createStatement();
-            ResultSet rs=stat.executeQuery(sql);
-            while(rs.next()){
-                System.out.print(rs.getString(1)+"\t\t");
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {
+                System.out.print(rs.getString(1) + "\t\t");
                 System.out.print(rs.getString(2));
                 System.out.println();
             }
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-	}
+    }
 
-	@Override
-	public void printPageInfo() {
+    @Override
+    public void printPageInfo() {
         System.out.print("\nInput the index of menu item to continue: \n");
         System.out.print("1 Transcript\n");
         System.out.print("2 Enroll\n");
         System.out.print("3 Withdraw\n");
         System.out.print("4 Personal Details\n");
         System.out.print("5 Logout\n");
-	}
+        System.out.print("6 Exit\n");
+    }
 
-	@Override
-	public MainLoop.Position execCommand(String command) throws IOException {
-        Set<String> set=new HashSet<>();
-        set.add("1");set.add("2");set.add("3");set.add("4");set.add("5");
-        MainLoop.Position position=null;
-		// some bad cases
-		if (!set.contains(command)) {
-			throw new IOException();
-		}
-		// exit case
-        switch (command){
-            case "1":
-                position=MainLoop.Position.TRANSCRIPT;
-                break;
-            case "2":
-                position= MainLoop.Position.ENROLL;
-                break;
-            case "3":
-                position=MainLoop.Position.WITHDRAW;
-                break;
-            case"4":
-                position=MainLoop.Position.PERSONAL_DETAILS;
-                break;
-            case "5":
-                position=MainLoop.Position.STARTED;
-                break;
+    @Override
+    public MainLoop.Position execCommand(String command) throws IOException {
+        MainLoop.Position position = null;
+        // some bad cases
+        if (!map.containsKey(command)) {
+            throw new IOException();
         }
-        return position;
-	}
+        // exit case
+        return map.get(command);
+    }
 }
